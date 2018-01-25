45c8517c9e161309b45f5f937a0478823eafee95
uyoung-jeong <farandole09@unist.ac.kr>
2018-01-25 19:43:27 +0900

    test

--- main.py
+++ main.py
@@ -102,6 +102,7 @@ if args.predict is not None:
     label = train.predict(args.predict, cnn, text_field, label_field, args.cuda)
     print('\n[Text]  {}\n[Label] {}\n'.format(args.predict, label))
 elif args.test:
+    train_iter, dev_iter, test_iter = mr(text_field, label_field, device=-1, repeat=False)
     train.eval(test_iter, cnn, args)
     """
     try:
